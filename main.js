84626c11b22b8dbf4e35ca44e242e20911a7313b
developfraenk <developfraenk@gmail.com>
2019-08-13 12:13:44 +0200

    second commit

--- main.js
+++ main.js
@@ -7,15 +7,52 @@ const recorder = new SpeechRecognition();
 function addHumanText(text) {
   const chatContainer = document.createElement("div");
   chatContainer.classList.add("chat-container");
-
   const chatBox = document.createElement("p");
   chatBox.classList.add("voice2text");
-
   const chatText = document.createTextNode(text);
   chatBox.appendChild(chatText);
-
   chatContainer.appendChild(chatBox);
   return chatContainer;
+}
+
+function addBotText(text) {
+  const chatContainer1 = document.createElement("div");
+  chatContainer1.classList.add("chat-container");
+  chatContainer1.classList.add("darker");
+  const chatBox1 = document.createElement("p");
+  chatBox1.classList.add("voice2text");
+  const chatText1 = document.createTextNode(text);
+  chatBox1.appendChild(chatText1);
+  chatContainer1.appendChild(chatBox1);
+  return chatContainer1;
+}
+
+function botVoice(message) {
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = "Sorry, I did not understand that.";
+
+    if (message.includes('how are you')) {
+      speech.text = "I am fine, thanks. How are you?";
+    }
+
+    if (message.includes('fine')) {
+      speech.text = "Nice to hear that. How can I assist you today?";
+    }
+
+    if (message.includes('weather')) {
+      speech.text = "Of course. Where are you currently?";
+    }
+
+    if (message.includes('London')) {
+      speech.text = "It is 18 degrees and sunny.";
+    }
+
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+    window.speechSynthesis.speak(speech);
+    var element = document.getElementById("container");
+    element.appendChild(addBotText(speech.text));
 }
 
 recorder.onstart = () => {
@@ -23,12 +60,11 @@ recorder.onstart = () => {
 };
 
 recorder.onresult = (event) => {
-  //console.log(event);
   const resultIndex = event.resultIndex;
   const transcript = event.results[resultIndex][0].transcript;
-  //voice2text.textContent = transcript;
   var element = document.getElementById("container");
   element.appendChild(addHumanText(transcript));
+  botVoice(transcript);
 };
 
 voice.addEventListener('click', () =>{
